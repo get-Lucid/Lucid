6e201b1aee76cf23b3b964cc3a347f1f746f044e
get-lucid <pinionagent714@gmail.com>
2026-03-14 15:07:05 +0100

    remove temp script

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,10 @@ export GIT_COMMITTER_NAME=get-lucid
 export GIT_COMMITTER_EMAIL=pinionagent714@gmail.com
 export GIT_AUTHOR_NAME=get-lucid
 export GIT_AUTHOR_EMAIL=pinionagent714@gmail.com
+git rm -f fix.sh 2>/dev/null
 git add -A
 TREE=$(git write-tree)
 PARENT=$(git log -1 --format=%H)
-NEW=$(echo "add runtime api key prompt" | git commit-tree $TREE -p $PARENT)
+NEW=$(echo "remove temp script" | git commit-tree $TREE -p $PARENT)
 git update-ref refs/heads/main $NEW
 git push origin main
